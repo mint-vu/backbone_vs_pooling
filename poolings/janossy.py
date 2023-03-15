4ba7b92735c51330e948a0b87d53a41acf1a68fc
Xinran Liu <xinranliueva@gmail.com>
2023-03-15 17:20:08 -0500

    Update janossy.py

--- poolings/janossy.py
+++ poolings/janossy.py
@@ -8,11 +8,13 @@ class Janossy(nn.Module):
         self.k = first_k
 
     def forward(self, X):
+        sorted_indices = X[:, 0].sort()[1]
+        X = X[sorted_indices]
         total_num = X.shape[0]
         all_k_ary = torch.combinations(torch.arange(total_num), r=self.k)
         X_jp = []
         for perm in all_k_ary:
             X_jp.append(X[perm])
         X_jp = torch.stack(X_jp, dim=0)
-        
+
         return X_jp.mean(dim=0)
