3722167f0594d2449aa5cfed5ba247bd2503e1d3
Xinran Liu <xinranliueva@gmail.com>
2023-03-15 16:28:36 -0500

    Create janossy.py

---
+++ poolings/janossy.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn
+
+
+class Janossy(nn.Module):
+    def __init__(self, first_k):
+        super().__init__()
+        self.k = first_k
+
+    def forward(self, X):
+        total_num = X.shape[0]
+        all_k_ary = torch.combinations(torch.arange(total_num), r=self.k)
+        X_jp = []
+        for perm in all_k_ary:
+            X_jp.append(X[perm])
+        X_jp = torch.stack(X_jp, dim=0)
+        
+        return X_jp.mean(dim=0)
